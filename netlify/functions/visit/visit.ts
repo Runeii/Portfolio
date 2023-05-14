bcc305beb4ccde6058f2d341790a4493fc8b6960
Andrew Hill <contact@andrewthomashill.co.uk>
2023-05-14 18:45:30 +0200

    Deploy IFTT

--- netlify/functions/visit/visit.ts
+++ netlify/functions/visit/visit.ts
@@ -1,13 +1,11 @@
 import { Handler } from '@netlify/functions'
 
 export const handler: Handler = async (event, context) => {
-  // Korean handily uses YY/MM//DD
-  const time = new Intl.DateTimeFormat('en-GB', {
-    dateStyle: 'short',
-    timeStyle: 'medium',
-  }).format(Date.now());
-
-  console.log("Visit::", { ...event.queryStringParameters, ip: event.headers['client-ip'], time });
+  console.log(event)
+  fetch(`https://maker.ifttt.com/trigger/portfolio_visited/json/with/key/${process.env.IFTTT_KEY}`, {
+    method: 'POST',
+    body: JSON.stringify({ ...event.queryStringParameters, ip: event.headers['client-ip'] })
+  })
 
   return {
     statusCode: 200,
