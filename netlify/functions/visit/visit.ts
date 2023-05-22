1da0ad24c31832ccad7a80838d1d01b3eeb59d06
Andrew Hill <contact@andrewthomashill.co.uk>
2023-05-22 21:38:57 +0200

    Fix func

--- netlify/functions/visit/visit.ts
+++ netlify/functions/visit/visit.ts
@@ -2,8 +2,9 @@ import { Handler } from '@netlify/functions';
 import fetch from 'node-fetch';
 
 export const handler: Handler = async (event, context) => {
-  fetch(`https://maker.ifttt.com/trigger/portfolio_visited/json/with/key/${process.env.IFTTT_KEY}`, {
+  await fetch(`https://maker.ifttt.com/trigger/portfolio_visited/json/with/key/${process.env.IFTTT_KEY}`, {
     method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       visitor_id: event.queryStringParameters?.id,
       path: event.queryStringParameters?.path,
@@ -15,6 +16,15 @@ export const handler: Handler = async (event, context) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ ok: true, details: event })
+    body: JSON.stringify({
+      ok: true,
+      details: {
+        visitor_id: event.queryStringParameters?.id,
+        path: event.queryStringParameters?.path,
+        action: event.queryStringParameters?.type,
+        ip: event.headers['cf-connecting-ip'],
+        country: event.headers['cf-ipcountry']
+      }
+    })
   }
 }
