a273421642bde9ee0c70c0a9ee837c5f49c43e33
Andrew Hill <contact@andrewthomashill.co.uk>
2024-04-22 16:20:40 +0200

    Exclude

--- netlify/functions/visit.ts
+++ netlify/functions/visit.ts
@@ -4,6 +4,10 @@ export default async (req: Request) => {
   const promptsStore = getStore('visits');
   const url = new URL(req.url);
 
+  const ip = req.headers.get('cf-connecting-ip');
+  if (ip === '2a02:a210:20ba:d500::1005' || ip === '62.163.118.12') {
+    return new Response();
+  }
   const date = new Date()
   await promptsStore.setJSON(date.toUTCString(), {
     visitor_id: url.searchParams.get('id'),
