18a32a86226f7123fc72dc11d8c2a22607f02364
Andrew Hill <contact@andrewthomashill.co.uk>
2024-06-12 12:45:35 +0200

    Update content

--- netlify/functions/visit.ts
+++ netlify/functions/visit.ts
@@ -1,21 +1,36 @@
-import { getStore } from "@netlify/blobs";
+import { GoogleSpreadsheet } from "google-spreadsheet"; // NOTE: Pinned to v3 to avoid unnecessary google-auth-library dependency
 
-export default async (req: Request) => {
-  const promptsStore = getStore('visits');
-  const url = new URL(req.url);
+const writeToSheet = async (data: any) => {
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 
-  const ip = req.headers.get('cf-connecting-ip');
-  if (ip === '2a02:a210:20ba:d500::1005' || ip === '62.163.118.12') {
-    return new Response();
-  }
-  const date = new Date()
-  await promptsStore.setJSON(date.toUTCString(), {
-    visitor_id: url.searchParams.get('id'),
-    path: url.searchParams.get('path'),
-    action: url.searchParams.get('type'),
-    ip: req.headers.get('cf-connecting-ip'),
-    country: req.headers.get('cf-ipcountry')
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SHEET_EMAIL,
+    private_key: process.env.GOOGLE_SHEET_KEY?.replace(/\\n/gm, "\n") ?? '',
   });
 
-  return new Response();
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[0]
+  await sheet.addRow(data);
+}
+
+export default async (req: Request) => {
+  const url = new URL(req.url);
+
+  await writeToSheet({
+    date: new Date(),
+    ip: req.headers.get('cf-connecting-ip'),
+    path: url.searchParams.get('path') ?? '/',
+    action: url.searchParams.get('type'),
+    country: req.headers.get('cf-ipcountry'),
+    type: url.searchParams.get('type'),
+  })
+
+  return new Response(JSON.stringify(
+    {
+      statusCode: 200,
+      body: "R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+      headers: { "content-type": "image/gif" },
+      isBase64Encoded: true,
+    }
+  ));
 };
