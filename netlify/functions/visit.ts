6f676561983b85b3a076d347174b2eacdd50ff4d
Andrew Hill <contact@andrewthomashill.co.uk>
2024-06-12 12:56:30 +0200

    Add logs

--- netlify/functions/visit.ts
+++ netlify/functions/visit.ts
@@ -1,14 +1,18 @@
 import { GoogleSpreadsheet } from "google-spreadsheet"; // NOTE: Pinned to v3 to avoid unnecessary google-auth-library dependency
 
 const writeToSheet = async (data: any) => {
+  console.log('start');
   const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 
+  console.log('connected');
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SHEET_EMAIL,
     private_key: process.env.GOOGLE_SHEET_KEY?.replace(/\\n/gm, "\n") ?? '',
   });
 
+  console.log('auth');
   await doc.loadInfo();
+  console.log('load');
   const sheet = doc.sheetsByIndex[0]
   await sheet.addRow(data);
 }
